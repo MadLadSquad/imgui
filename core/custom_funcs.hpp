61e1275a9ae56ceb08f2ac5e0c593ce3e7b37433
Madman10K <stanv.contact176@gmail.com>
2024-06-07 22:57:58 +0300

    Fix nullptr redefinition errors with default arguments in custom_funcs.hpp

--- core/custom_funcs.hpp
+++ core/custom_funcs.hpp
@@ -4,15 +4,15 @@
 namespace ImGui
 {
     // Modified ShowDemoWindow that uses void instead of bool for p_open
-    IMGUI_API void          ShowDemoWindow(void* p_open = nullptr);
+    IMGUI_API void          ShowDemoWindow(void* p_open);
     // Modified ShowMetricsWindow that uses void instead of bool for p_open
-    IMGUI_API void          ShowMetricsWindow(void* p_open = nullptr);
+    IMGUI_API void          ShowMetricsWindow(void* p_open);
     // Modified ShowDebugLogWindow that uses void instead of bool for p_open
-    IMGUI_API void          ShowDebugLogWindow(void* p_open = nullptr);
+    IMGUI_API void          ShowDebugLogWindow(void* p_open);
     // Modified ShowStackToolWindow that uses void instead of bool for p_open
-    IMGUI_API void          ShowStackToolWindow(void* p_open = nullptr);
+    IMGUI_API void          ShowStackToolWindow(void* p_open);
     // Modified ShowAboutWindow that uses void instead of bool for p_open
-    IMGUI_API void          ShowAboutWindow(void* p_open = nullptr);
+    IMGUI_API void          ShowAboutWindow(void* p_open);
 
     // Modified Checkbox that uses void instead of bool for v
     IMGUI_API void          Checkbox(const char* name, void* v);
@@ -27,11 +27,11 @@ namespace ImGui
     IMGUI_API bool          MenuItem(const char* label, const char* shortcut, void* p_selected, bool enabled = true);
 
     // Modified Begin that uses void instead of bool for p_open
-    IMGUI_API bool          Begin(const char* name, void* p_open = nullptr, ImGuiWindowFlags flags = 0);
+    IMGUI_API bool          Begin(const char* name, void* p_open, ImGuiWindowFlags flags = 0);
 
     // Modified BeginPopupModal that uses void instead of bool for p_open
-    IMGUI_API bool          BeginPopupModal(const char* name, void* p_open = nullptr, ImGuiWindowFlags flags = 0);
+    IMGUI_API bool          BeginPopupModal(const char* name, void* p_open, ImGuiWindowFlags flags = 0);
 
     // Modified BeginTabItem that uses void instead of bool for p_open
-    IMGUI_API bool          BeginTabItem(const char* label, void* p_open = nullptr, ImGuiTabItemFlags flags = 0);
+    IMGUI_API bool          BeginTabItem(const char* label, void* p_open, ImGuiTabItemFlags flags = 0);
 }
