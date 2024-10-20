ef62f540e9e1faee440fd6936b7c783a5c9ec5e1
Madman10K <stanv.contact176@gmail.com>
2024-10-20 03:18:08 +0300

    Minor improvements to our custom functions

--- core/custom_funcs.cpp
+++ core/custom_funcs.cpp
@@ -2,60 +2,60 @@
 
 void ImGui::ShowDemoWindow(void* p_open)
 {
-    ShowDemoWindow((bool*)p_open);
+    ShowDemoWindow(static_cast<bool*>(p_open));
 }
 
 void ImGui::ShowMetricsWindow(void* p_open)
 {
-    ShowMetricsWindow((bool*)p_open);
+    ShowMetricsWindow(static_cast<bool*>(p_open));
 }
 
 void ImGui::ShowDebugLogWindow(void* p_open)
 {
-    ShowDebugLogWindow((bool*)p_open);
+    ShowDebugLogWindow(static_cast<bool*>(p_open));
 }
 
 void ImGui::ShowIDStackToolWindow(void* p_open)
 {
-    ShowIDStackToolWindow((bool*)p_open);
+    ShowIDStackToolWindow(static_cast<bool*>(p_open));
 }
 
 void ImGui::ShowAboutWindow(void* p_open)
 {
-    ShowAboutWindow((bool*)p_open);
+    ShowAboutWindow(static_cast<bool*>(p_open));
 }
 
 void ImGui::Checkbox(const char* name, void* v)
 {
-    ImGui::Checkbox(name, (bool*)v);
+    ImGui::Checkbox(name, static_cast<bool*>(v));
 }
 
-bool ImGui::CollapsingHeader(const char* label, void* p_visible, ImGuiTreeNodeFlags flags)
+bool ImGui::CollapsingHeader(const char* label, void* p_visible, const ImGuiTreeNodeFlags flags)
 {
-    return ImGui::CollapsingHeader(label, (bool*)p_visible, flags);
+    return ImGui::CollapsingHeader(label, static_cast<bool*>(p_visible), flags);
 }
 
-bool ImGui::Selectable(const char* label, void* p_selected, ImGuiSelectableFlags flags, const ImVec2& size)
+bool ImGui::Selectable(const char* label, void* p_selected, const ImGuiSelectableFlags flags, const ImVec2& size)
 {
-    return ImGui::Selectable(label, (bool*)p_selected, flags, size);
+    return ImGui::Selectable(label, static_cast<bool*>(p_selected), flags, size);
 }
 
-bool ImGui::MenuItem(const char* label, const char* shortcut, void* p_selected, bool enabled)
+bool ImGui::MenuItem(const char* label, const char* shortcut, void* p_selected, const bool enabled)
 {
-    return ImGui::MenuItem(label, shortcut, (bool*)p_selected, enabled);
+    return ImGui::MenuItem(label, shortcut, static_cast<bool*>(p_selected), enabled);
 }
 
-bool ImGui::Begin(const char* name, void* p_open, ImGuiWindowFlags flags)
+bool ImGui::Begin(const char* name, void* p_open, const ImGuiWindowFlags flags)
 {
-    return Begin(name, (bool*)p_open, flags);
+    return Begin(name, static_cast<bool*>(p_open), flags);
 }
 
-bool ImGui::BeginPopupModal(const char* name, void* p_open, ImGuiWindowFlags flags)
+bool ImGui::BeginPopupModal(const char* name, void* p_open, const ImGuiWindowFlags flags)
 {
-    return BeginPopupModal(name, (bool*)p_open, flags);
+    return BeginPopupModal(name, static_cast<bool*>(p_open), flags);
 }
 
-bool ImGui::BeginTabItem(const char* label, void* p_open, ImGuiTabItemFlags flags)
+bool ImGui::BeginTabItem(const char* label, void* p_open, const ImGuiTabItemFlags flags)
 {
-    return BeginTabItem(label, (bool*)p_open, flags);
+    return BeginTabItem(label, static_cast<bool*>(p_open), flags);
 }
